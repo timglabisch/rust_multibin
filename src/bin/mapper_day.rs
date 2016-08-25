42de84a67c4e0792d42cda99a581c56e56c49219
timglabisch <tim@ainfach.de>
2016-08-25 12:11:55 +0200

    ...

---
+++ src/bin/mapper_day.rs
@@ -0,0 +1,8 @@
+extern crate common;
+use common::foo;
+
+fn main() {
+    foo::foo();
+
+    println!("Hello, world!");
+}
---
+++ src/common/foo.rs
@@ -0,0 +1,5 @@
+use std::collections::HashMap;
+
+pub fn foo() {
+    println!("fooo!");
+}
